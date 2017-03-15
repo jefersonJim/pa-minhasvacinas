828a8107c9d531b49ee96f1b9b63192aa2f7248f
Roberto Evangelista <betoesf@gmail.com>
2017-03-14 21:12:54 -0300

    Config Ambiente, LoginComponnet e Cadastro-PrincipalComponent

---
+++ frontend/src/app/cadastro-principal/cadastro-principal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-principal',
+  templateUrl: './cadastro-principal.component.html',
+  styleUrls: ['./cadastro-principal.component.scss']
+})
+export class CadastroPrincipalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
